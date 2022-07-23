e53ca2e080e606dff64733efdbee2cceac0417d1
Bryan Marfito <bjmarfito@yahoo.com>
2022-07-23 13:50:04 +0800

    Update loadLOS_MINTPY.m

--- DataLoaders/loadLOS_MINTPY.m
+++ DataLoaders/loadLOS_MINTPY.m
@@ -1,5 +1,5 @@
 %Modified for MintPy outputs
-function datastruct = loadLOS_ROIPAC(datastruct,losfilename,azo,const_los)
+function datastruct = loadLOS_MINTPY(datastruct,losfilename,azo,const_los)
 
 if(azo==1)
     % changed to use heading from input .rsc file EJF 2010/4/29
