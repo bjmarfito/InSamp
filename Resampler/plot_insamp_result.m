14dc9cc1b5835d7d4de8c5f6113169a760f429b9
Bryan Marfito <bjmarfito@yahoo.com>
2022-07-29 09:54:54 +0800

    Add files via upload

---
+++ Resampler/plot_insamp_result.m
@@ -0,0 +1,30 @@
+%Plot sampled data location points
+data = [savestruct.data.data];
+X = [savestruct.data.X];
+Y = [savestruct.data.Y];
+k =  numel(data);
+
+figure
+scatter(X,Y,24,data,'filled')
+axis image; shading flat
+c = colorbar;
+c.Label.String = 'LOS displacement (m)';
+c.Location = 'southoutside';
+c.Ticks = [-0.15 -0.10 -0.05 0 0.05 0.10 0.15 0.20 0.25 0.30 0.35];
+title(['No. of points = ' num2str(k)])
+
+
+%Plot resampled data
+data = [savestruct.data.data];
+trix = [savestruct.data.trix];
+triy = [savestruct.data.triy];
+k =  numel(data);
+
+figure
+patch(trix,triy,data)
+axis image; shading flat
+c = colorbar;
+c.Label.String = 'LOS displacement (m)';
+c.Location = 'southoutside';
+c.Ticks = [-0.15 -0.10 -0.05 0 0.05 0.10 0.15 0.20 0.25 0.30 0.35];
+title(['No. of points = ' num2str(k)])
---
+++ Resampler/save_downsampled_InSamp.m
@@ -0,0 +1,23 @@
+%Transform InSAMP data from UTM to lon latm and dump data, and enu vectors into a text file
+%with a format lon lat east west up dlos
+load()
+X = [savestruct.data.X];
+Y = [savestruct.data.Y];
+data = [savestruct.data.data];
+%Create an array for the lon lat and utm
+[m, n] = size(X);
+lon = ones(m,n);
+lat = ones(m,n);
+utmZ = savestruct.zone(1:2);
+
+for a = m:n
+    [lon(m,a) lat(m,a)]= utm2ll(X(m,a), Y(m,a),42, 2);
+end
+
+lon = transpose(lon);
+lat = transpose(lat);
+enuVector = [savestruct.data.S];
+enuVector = transpose(enuVector) .* -1;
+data = transpose(data);
+vectormatrix = [lon lat enuVector data];
+writematrix(vectormatrix, 'S1_asc_downsampled.txt', 'Delimiter','space')
