e2b0ef994db6b729e1560a5c79fc4e48b1f229b1
#MARFITO BRYAN JIMENEZ# <bjmarfito@github.com>
2022-10-22 17:25:31 +0800

    Revert "Changes to InSamp"
    This reverts commit 974c2f7de5989116c9d218611f1d656b755102e2.

--- DataLoaders/loadMINTPYISCE.m
+++ DataLoaders/loadMINTPYISCE.m
@@ -1,6 +1,12 @@
 %Modified for MintPy outputs
 function [datastruct]=loadMINTPYISCE(filename,zone,limitny,azo,scaleval)
-pathname =[];
+if(exist([matlabroot '/toolbox/map/mapproj'])==7)
+    goodmap=1;
+else
+    disp('No mapping toolbox found, using clugy utm2ll for lat/lon conversion')
+    goodmap=0;
+end
+
 datastruct=struct([]);
 if(nargin<5)
     scaleval=0;
@@ -15,128 +21,114 @@ if(nargin<2)
     zone=0;
 end
 if(nargin<1)
-    [infilename, pathname]=uigetfile({'*.geo','Unwrapped files (*.unw)'; ...
-        '*.flat','Interferograms (*.flat)'; ...
+    [infilename, pathname]=uigetfile({'*.unw','Unwrapped files (*.unw)'; ...
+        '*.int','Interferograms (*.int)'; ...
         '*.slc','Single Look Complex (*.slc)'; ...
-        %         '*.amp','Amplitude files (*.amp)'; ...
+        '*.amp','Amplitude files (*.amp)'; ...
         '*.cor','Correlation files (*.cor)'; ...
         '*.hgt','Height files (*.hgt)'; ....
-        %         '*.msk','Mask files (*.msk)'; ...
+        '*.msk','Mask files (*.msk)'; ...
         '*.dem','DEMs (*.dem)'; ...
-        %         '*.byt','Byte files (*.byt)'; ...
-        %         '*.flg','Flag files (*.flg)'; ...
-        %         '*.slp','Slope files (*.slp)'; ...
-        %         '*.off','Offset files (*.off)'; ...
+        '*.byt','Byte files (*.byt)'; ...
+        '*.flg','Flag files (*.flg)'; ...
+        '*.slp','Slope files (*.slp)'; ...
+        '*.off','Offset files (*.off)'; ...
         '*','All files'}, ...
         'Pick an input file');
-    
+
     filename=[pathname infilename];
-elseif length(strsplit(filename,'/'))>1
-    pathname = [];
-    parts=strsplit(filename,'/');
-    if isempty(parts{1})
-        for k=2:length(parts)-1
-            pathname = [pathname  '/' parts{k}];
-        end
-        %         pathname = ['/' pathname];
-    else
-        for k=1:length(parts)-2
-            pathname =[pathname parts{k} '/'];
-        end
-        pathname = [pathname parts{end-1}];
-    end
-else
-    pathname = '.';
 end
-% if isempty(pathname)
-%     pathname='.';
-% end
+
 mag=[];
 phs=[];
+pixelsize=[];
+
+
+[nx,ny,lambda,x1,y2,dx,dy,xunit]=load_rscs(filename,'WIDTH','FILE_LENGTH','WAVELENGTH','X_FIRST','Y_FIRST','X_STEP','Y_STEP','X_UNIT');
 
 n        = length(filename);
-patterns = {'phsig','cor','unw','rdr','flat','hgt','dem','slc'};
-figtype  = {'phsig','rmg','rmg','rmg','cpx','rmg','rmg','cpx'};
-
-
-
-for k = 1:length(patterns)
-    if(regexp(filename,patterns{k}))
-        type=figtype{k};
-        break
-    end
-end
+patterns = {'.int','.slc','.amp','.cor','.unw','.hgt','.msk','.dem','.byt','.flg','.slp','.off','.vel'};
+type     = strmatch(filename(n-3:end),patterns);
 im       = sqrt(-1);
 
-if(regexp(filename,'geo'))
-    %     [nx,ny,lambda,x1,y2,dx,dy] = load_xml([pathname '/insarProc.xml'],'GEO_WIDTH','GEO_LENGTH','radar_wavelength','maximum_longitude','maximum_latitude','LONGITUDE_SPACING','LATITUDE_SPACING');
-    %     [nx,ny,x1,y2,dx,dy] = load_xml([pathname '/' filename '.xml'],
-    [nx,ny,x1,y2,dx,dy] = loadGeoXml([filename '.xml']);
-    %if regexp(filename,'merged')
-        %lambda= '0.055465763';
-    %elseif regexp(filename,'insar')
-        lambda = '0.2424525';
-    %else
-        %lambda  = loadGenericXml([pathname '/insarProc.xml'],'radar_wavelength');
-        % lambda='0.055';
-    %end
-    
-    nx = str2num(nx); ny = str2num(ny); x1 = str2num(x1); y2 = str2num(y2); dx = str2num(dx); dy = str2num(dy);lambda=str2num(lambda);
-else
-    %if regexp(filename,'merged')
-        
-        %lambda = '0.05465763';
-        %[nx, ny] = loadISCEinfo([pathname(1:end-7) '/isce.log'],'isce.mroipac.filter - DEBUG - width','isce.mroipac.filter - DEBUG - length');
-        
-    %else
-        
-        %     [nx,ny,lambda] = loadGenericXml([pathname '/insarProc.xml'],'WIDTH','LENGTH','RADAR_WAVELENGTH');
-        %[nx, ny,lambda] = loadISCEinfo([pathname '/isce.log'],'runCorrect.inputs.width','runCorrect.inputs.length','runCorrect.inputs.radar_wavelength');
-    end
-    nx = str2num(nx); ny = str2num(ny); lambda =str2num(lambda);
-end
-lambda = '1';
-disp("Making wavelength to 1 meter for this files")
-wvlVal = strcat('Wavelength (m): ',num2str(lambda));
-disp(wvlVal)
-% lambda  = getWavelength(sensor);
-
 if(limitny)
-    ny = limitny;
+    ny=limitny;
 end
 
-switch type
-    case 'rmg'
-        disp('Loading rmg file')
-        fid             = fopen(filename,'r','native');
-        [rmg,count]     = fread(fid,[nx,ny*2],'real*4');
-        [nx,ny]         = size(rmg);
-        ny              = ny/2;
-        status          = fclose(fid);
-        mag             = flipud((rmg(1:nx,1:2:ny*2))');
-        phs             = flipud((rmg(1:nx,2:2:ny*2))');
-        data            = phs;
-        if(regexp(filename,'unw'));
-            data            = -phs*lambda/(4*pi);
-        end
-    case 'cpx'
-        
-        disp('Loading cpx file')
-        fid         = fopen(filename,'r','native');
-        [rmg,count] = fread(fid,[nx*2,ny],'real*4');
-        status      = fclose(fid);
-        real        = flipud((rmg(1:2:nx*2,1:ny))');
-        imag        = flipud((rmg(2:2:nx*2,1:ny))');
-        mag         = abs(real+im*imag);
-        phs         = angle(real+im*imag);
-        data        = phs;
-    case 'phsig'
-        disp('Loading PHSIG file')
-        fid=fopen(filename,'r','native');
-        [cor,count]=fread(fid,[nx, ny],'real*4');
-        data = flipud(cor');
-end
+if(sum(type==[1:3])) %cpx file
+  disp('Loading cpx file')
+  fid         = fopen(filename,'r','native');
+  [rmg,count] = fread(fid,[nx*2,ny],'real*4');
+  status      = fclose(fid);
+  real        = flipud((rmg(1:2:nx*2,1:ny))');
+  imag        = flipud((rmg(2:2:nx*2,1:ny))');
+  mag         = abs(real+im*imag);
+  phs         = angle(real+im*imag);
+  data        = phs;
+  
+elseif(sum(type==[4:7])) %rmg
+  disp('Loading rmg file')
+  fid         = fopen(filename,'r','native');
+  [rmg,count] = fread(fid,[nx,ny*2],'real*4');
+  status      = fclose(fid);
+  mag         = flipud((rmg(1:nx,1:2:ny*2))');
+  %Modified for MintPy outputs
+  mag(isnan(mag))=0;
+  phs         = flipud((rmg(1:nx,2:2:ny*2))');
+  %Modified for MintPy outputs
+  phs(isnan(phs))=0;
+  data        = phs;
+  
+  if(azo)
+      %azimuth offset type
+      disp('just using offsets, no scale')
+  else
+      if(type==5) %unw file
+          if(lambda==0)
+              disp('not scaling by lambda')
+          else
+              data  = -phs*lambda/(4*pi); 
+              data  = data;
+              disp(lambda)
+%             data = data;
+          end
+      end
+  end
 
+elseif(sum(type==[8:9])) %i2
+  disp('Loading i2 file')
+  fid          = fopen(filename,'r','native');
+  [data,count] = fread(fid,[nx,ny],'integer*2');
+  status       = fclose(fid);
+  data         = flipud(data');
+  
+elseif(type==10) %i1
+  disp('Loading i1 file')
+  fid          = fopen(filename,'r','native');
+  [data,count] = fread(fid,[nx,ny],'integer*1');
+  status       = fclose(fid);
+  data         = flipud(data');
+  
+elseif(type==11) %slope
+  disp('Loading slope file')
+  
+  fid          = fopen(filename,'r','native');
+  [rmg,count]  = fread(fid,[nx*2,ny],'real*4');
+  status       = fclose(fid);
+  slope        = flipud((rmg(1:2:nx*2,1:ny))')*180/pi;
+  azimuth      = flipud((rmg(2:2:nx*2,1:ny))')*180/pi;
+  data         = slope;
+  
+elseif(type==12)%off
+  disp('Loading offset file')
+  fid          = fopen(filename,'r','native');
+  [data,count] = fread(fid,[nx,ny],'real*4');
+  status       = fclose(fid);
+  
+else
+  disp('filename has bad format, must end with .int, .slc, .amp, .cor, .unw, .hgt, .msk, .dem, .byt,.flg, .slp, or .off');
+  return
+end
 
 
 if(scaleval)
@@ -155,37 +147,54 @@ else
 end
 
 
-if(regexp(filename,'geo'))
+pixelsize=[];
+if (isnumeric(x1))
     disp('File is geocoded')
-    y1      =y2+dy*(ny-1);
-    x2      =x1+dx*(nx-1);
-    
-    x       =x1:dx:x2;
-    y       =y1:-dy:y2;
-    
-    [X,Y]    =meshgrid(x,y);
-    if zone==0
-        [jnk1,jnk2,zone] = my_utm2ll(mean(x),mean(y),2);
-    end
-    if(str2num(zone)<=-1)
-        disp('not setting zone');
+   
+    y1=y2+dy*(ny-1);
+    x2=x1+dx*(nx-1);
+
+    x=x1:dx:x2;
+    y=y1:-dy:y2;
+
+    [X,Y]=meshgrid(x,y);
+    if(strcmp(xunit,'meters'))
+        pixelsize=mean([sqrt((X(1)-X(2))^2+(Y(1)-Y(2))^2) sqrt((X(nx*ny-1)-X(nx*ny))^2+(Y(nx*ny-1)-Y(nx*ny))^2)]);
+        X=X+pixelsize/2;
+        Y=Y-pixelsize/2;
+        zone=[];
     else
-        [leng,width]        =size(X);
-        [X,Y]               = my_utm2ll(X,Y,2,zone);
-        X                   = reshape(X,leng,width);
-        Y                   = reshape(Y,leng,width);
-        pixelsize           = mean([sqrt((X(1)-X(2))^2+(Y(1)-Y(2))^2) sqrt((X(nx*ny-1)-X(nx*ny))^2+(Y(nx*ny-1)-Y(nx*ny))^2)]);
-        X                   = X+pixelsize/2;
-        Y                   = Y-pixelsize/2;
+        if(zone)
+        else
+            if(goodmap)
+                [jnk1,jnk2,zone]=my_utm2ll(mean(x),mean(y),2);
+            else
+                [jnk1,jnk2,zone]=utm2ll(mean(x),mean(y),0,2);
+            end
+            zone=char(inputdlg(['which zone (-1=nogeo), ' num2str(zone) '?']));
+
+        end
+        if(str2num(zone)<=-1)
+            disp('not setting zone')
+        else
+            if(goodmap)
+                [X,Y]=my_utm2ll(X,Y,2,zone);
+            else
+                [X,Y]=utm2ll(X,Y,2,zone);
+            end
+            pixelsize=mean([sqrt((X(1)-X(2))^2+(Y(1)-Y(2))^2) sqrt((X(nx*ny-1)-X(nx*ny))^2+(Y(nx*ny-1)-Y(nx*ny))^2)]);
+            X=X+pixelsize/2;
+            Y=Y-pixelsize/2;
+
+        end
     end
 else
     disp('File is not geocoded')
-    [X,Y]   = meshgrid(1:nx,1:ny);
-    zone    = [];
+    [X,Y]=meshgrid(1:nx,1:ny);
+    zone=[];
 end
 
-pixelsize=mean([sqrt((X(1)-X(2))^2+(Y(1)-Y(2))^2) sqrt((X(nx*ny-1)-X(nx*ny))^2+(Y(nx*ny-1)-Y(nx*ny))^2)]);
-
+%look for bad points
 baddata         = mode(data(:));
 badid           = find(data==baddata);
 data(badid)     = NaN;
@@ -195,4 +204,3 @@ disp(['setting ' num2str(length(badid)) ' pts with phs=' num2str(baddata) ' to N
 datastruct=struct('data',data,'mag',mag,'phs',phs,'X',X,'Y',Y,'pixelsize',pixelsize, ...
     'zone',zone,'lambda',lambda,'nx',nx,'ny',ny,'filename',filename, ...
     'scale',scale','extrax',extrax,'extray',extray);
-
