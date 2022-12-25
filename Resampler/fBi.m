c0c1de23590db6191a918054ebb8ac4657d04f50
Bryan Marfito <bjmarfito@yahoo.com>
2022-12-25 21:38:57 +0800

    Update fBi.m

--- Resampler/fBi.m
+++ Resampler/fBi.m
@@ -11,17 +11,17 @@ fault_type = parvec(4);
 %%%%% Some simplifications %%%%%
 
 epsn  = 1.0e-10;
-cosd  = cos(delta);
-sind  = sin(delta);
-tand  = tan(delta);
+cosDelta  = cos(delta);
+sinDelta  = sin(delta);
+tanDelta  = tan(delta);
 cosd2 = cos(delta)^2;
 sind2 = sin(delta)^2;
 cssnd = cos(delta)*sin(delta);
 
 R     = sqrt(sig.^2 + eta.^2 + q.^2);	
 X     = sqrt(sig.^2 + q.^2);		
-ytil  = eta*cosd + q*sind;		
-dtil  = eta*sind - q*cosd;		
+ytil  = eta*cosDelta + q*sinDelta;		
+dtil  = eta*sinDelta - q*cosDelta;		
 
 Rdtil = R + dtil;			
 Rsig  = R + sig;			
@@ -67,30 +67,30 @@ if (~isempty(indfix))
 end
 %%%%% The I_12345 factors %%%%%
 
-if abs(cosd) < epsn
-%%%%% cosd = 0 fix [eqn. (29)]
-  I5 = -a   .* sig .* sind ./ Rdtil;
+if abs(cosDelta) < epsn
+%%%%% cosDelta = 0 fix [eqn. (29)]
+  I5 = -a   .* sig .* sinDelta ./ Rdtil;
   I4 = -a   .* q ./ Rdtil;
   I3 =  a/2 .* (eta ./ Rdtil + (ytil .* q) ./ (Rdtil.^2) - lnReta );
   I2 = -a   .* lnReta - I3;
   I1 = -a/2 .* (sig .* q) ./ (Rdtil.^2);
 else
 %%%%% default [eqn. (28)]
-%  I5 = a * 2 ./ cosd .* atan2( (eta.*(X+q.*cosd) + X.*RX.*sind),...
-%                             (sig.*RX.*cosd) );
+%  I5 = a * 2 ./ cosDelta .* atan2( (eta.*(X+q.*cosDelta) + X.*RX.*sinDelta),...
+%                             (sig.*RX.*cosDelta) );
   sigtemp = sig;
   indfix = find(abs(sig)<epsn);
   sigtemp(indfix) = epsn;
-  I5 = a * 2 ./ cosd .* ...
-       atan( (eta.*(X+q.*cosd) + X.*RX.*sind)./(sigtemp.*RX.*cosd) );
+  I5 = a * 2 ./ cosDelta .* ...
+       atan( (eta.*(X+q.*cosDelta) + X.*RX.*sinDelta)./(sigtemp.*RX.*cosDelta) );
   if (~isempty(indfix))
    I5(indfix) = 0 * indfix;
   end
   
-  I4 = a ./ cosd .* (lnRdtil - sind .* lnReta);
-  I3 = a * (1 ./ cosd .* ytil ./ Rdtil - lnReta) + tand .* I4;
+  I4 = a ./ cosDelta .* (lnRdtil - sinDelta .* lnReta);
+  I3 = a * (1 ./ cosDelta .* ytil ./ Rdtil - lnReta) + tanDelta .* I4;
   I2 = -a .* lnReta - I3;
-  I1 = -a ./ cosd .* sig ./ Rdtil - tand .* I5;
+  I1 = -a ./ cosDelta .* sig ./ Rdtil - tanDelta .* I5;
 end
 
 %change shape if necessary
@@ -105,23 +105,23 @@ end
 
 if fault_type == 1;  		%%%%% Strike Slip [eqn. (25)]
 
-  f1 = (sig .* q)  .* ORReta + theta + I1 .* sind;
-  f2 = (ytil .* q) .* ORReta + (q .* cosd) .* OReta + I2 .* sind;
-  f3 = (dtil .* q) .* ORReta + (q .* sind) .* OReta + I4 .* sind;
+  f1 = (sig .* q)  .* ORReta + theta + I1 .* sinDelta;
+  f2 = (ytil .* q) .* ORReta + (q .* cosDelta) .* OReta + I2 .* sinDelta;
+  f3 = (dtil .* q) .* ORReta + (q .* sinDelta) .* OReta + I4 .* sinDelta;
 %  f3 = -f3; %fix by rlohman?
 elseif fault_type == 2;  	%%%%% Dip Slip  [eqn. (26)]
 
   f1 = q./R - I3 .* cssnd;
-  f2 = (ytil .* q) .* ORRsig + cosd .* theta - I1 .* cssnd;
-  f3 = (dtil .* q) .* ORRsig + sind .* theta - I5 .* cssnd;
+  f2 = (ytil .* q) .* ORRsig + cosDelta .* theta - I1 .* cssnd;
+  f3 = (dtil .* q) .* ORRsig + sinDelta .* theta - I5 .* cssnd;
 
 else fault_type == 3;		%%%%% Tensile [eqn. (27)]
 
-  f1 = q.^2 .* ORReta - I3 .* sind2;
+  f1 = q.^2 .* ORReta - I3 .* sinDelta2;
   f2 = (-dtil .* q) .* ORRsig ...
-     - sind .* ((sig .* q) .* ORReta - theta) - I1 .* sind2;
+     - sinDelta .* ((sig .* q) .* ORReta - theta) - I1 .* sinDelta2;
   f3 = (ytil .* q) .* ORRsig ...
-     + cosd .* ((sig .* q) .* ORReta - theta) - I5 .* sind2;
+     + cosDelta .* ((sig .* q) .* ORReta - theta) - I5 .* sinDelta2;
  % f3=-f3;%fix by rlohman?
 end
 
