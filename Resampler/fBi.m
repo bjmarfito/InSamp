46164416c9a64a55c36f267cec1885a6f22a3739
Bryan Marfito <bjmarfito@yahoo.com>
2022-12-25 21:35:05 +0800

    Update fBi.m

--- Resampler/fBi.m
+++ Resampler/fBi.m
@@ -1,36 +1,46 @@
 function [f1,f2,f3] = fBi(sig, eta, parvec, p, q);
 
-% Pull out parameters form parvec
-% parvec = [a, delta, fault_type]
+%%%%% Pull out parameters form parvec    %%%%%
+%%%%% parvec = [d, a, delta, fault_type] %%%%%
 
-a          = parvec(1);
-delta      = parvec(2);
-fault_type = parvec(3);
+d          = parvec(1);
+a          = parvec(2);
+delta      = parvec(3);
+fault_type = parvec(4);
 
-%
+%%%%% Some simplifications %%%%%
 
-epsn  = 1.0e-15;
-cosDelta  = cos(delta);
-sinDelta  = sin(delta);
+epsn  = 1.0e-10;
+cosd  = cos(delta);
+sind  = sin(delta);
 tand  = tan(delta);
-cosDelta2 = cos(delta)^2;
-sinDelta2 = sin(delta)^2;
+cosd2 = cos(delta)^2;
+sind2 = sin(delta)^2;
 cssnd = cos(delta)*sin(delta);
 
-R     = sqrt(sig.^2 + eta.^2 + q.^2);
+R     = sqrt(sig.^2 + eta.^2 + q.^2);	
 X     = sqrt(sig.^2 + q.^2);		
-ytil  = eta*cosDelta + q*sinDelta;		
-dtil  = eta*sinDelta - q*cosDelta;		
+ytil  = eta*cosd + q*sind;		
+dtil  = eta*sind - q*cosd;		
 
 Rdtil = R + dtil;			
 Rsig  = R + sig;			
 Reta  = R + eta;			
 RX    = R + X;
-			
+
 lnRdtil = log(Rdtil);		
-lnReta  = log(Reta);	
+lnReta  = log(Reta);
+
+badid   = find((R-eta)==0);
 lnReta0 = -log(R-eta);	
-ORRsig  = 1 ./ (R .* Rsig);			
+lnReta0(badid) = -inf;
+%lnReta0 = lnReta0';
+
+badid  = find(or(R==0,Rsig==0));
+ORRsig = 1./(R.*Rsig);
+ORRsig(badid)=inf;
+%ORRsig = ORRsig';
+
 OReta   = 1 ./ Reta;			
 ORReta  = 1 ./ (R .* Reta);
 
@@ -48,69 +58,70 @@ if (~isempty(indfix))
 end
 
 %%%%% theta term with q = 0 fix
-
-%betatop=sig.*eta;
-%betabottom=q.*R;
-%nz=find(betabottom~=0);
-%theta=pi/2*sign(betatop);
-%theta(nz)=atan(betatop(nz)./betabottom(nz));
-%theta(find(abs(q) > epsn))  = atan((sig.*eta)./(q.*R));		
-indfix = find(abs(q) <= epsn);
+theta  = 0*q;
+indfix = find(abs(q) < epsn);
+indok  = find(abs(q) > epsn);
+theta(indok)  = atan((sig(indok).*eta(indok))./(q(indok).*R(indok)));		
 if (~isempty(indfix))
   theta(indfix) = 0 * indfix;
-  indfix = find(abs(q) > epsn);
-  theta(indfix) = atan((sig(indfix).*eta(indfix))./(q(indfix).*R(indfix)));
- else
-  theta = atan((sig.*eta)./(q.*R));
 end
-
 %%%%% The I_12345 factors %%%%%
 
-if abs(cosDelta) < epsn
-%%%%% cosDelta = 0 fix [eqn. (29)]
-  I5 = -a   .* sig .* sinDelta ./ Rdtil;
+if abs(cosd) < epsn
+%%%%% cosd = 0 fix [eqn. (29)]
+  I5 = -a   .* sig .* sind ./ Rdtil;
   I4 = -a   .* q ./ Rdtil;
   I3 =  a/2 .* (eta ./ Rdtil + (ytil .* q) ./ (Rdtil.^2) - lnReta );
   I2 = -a   .* lnReta - I3;
   I1 = -a/2 .* (sig .* q) ./ (Rdtil.^2);
 else
 %%%%% default [eqn. (28)]
-%   sigtemp(indfix) = epsn;
-%  I5 = a * 2 ./ cosDelta .* atan2( (eta.*(X+q.*cosDelta) + X.*RX.*sinDelta),...
-%                             (sig.*RX.*cosDelta) );
-%%  indfix = find(abs(sig)<epsn);
-   I5 = a * 2 ./ cosDelta .* ...
-     atan( (eta.*(X+q.*cosDelta) + X.*RX.*sinDelta)./(sig.*RX.*cosDelta) ); 
+%  I5 = a * 2 ./ cosd .* atan2( (eta.*(X+q.*cosd) + X.*RX.*sind),...
+%                             (sig.*RX.*cosd) );
+  sigtemp = sig;
   indfix = find(abs(sig)<epsn);
+  sigtemp(indfix) = epsn;
+  I5 = a * 2 ./ cosd .* ...
+       atan( (eta.*(X+q.*cosd) + X.*RX.*sind)./(sigtemp.*RX.*cosd) );
   if (~isempty(indfix))
    I5(indfix) = 0 * indfix;
   end
-  I4 = a ./ cosDelta .* (lnRdtil - sinDelta .* lnReta);
-  I3 = a * (1 ./ cosDelta .* ytil ./ Rdtil - lnReta) + tand .* I4;
+  
+  I4 = a ./ cosd .* (lnRdtil - sind .* lnReta);
+  I3 = a * (1 ./ cosd .* ytil ./ Rdtil - lnReta) + tand .* I4;
   I2 = -a .* lnReta - I3;
-  I1 = -a ./ cosDelta .* sig ./ Rdtil - tand .* I5;
+  I1 = -a ./ cosd .* sig ./ Rdtil - tand .* I5;
 end
+
+%change shape if necessary
+%[ny,nx]=size(p);
+%if(and(ny==1,nx>1))
+%    theta   = theta';
+%    lnReta0 = lnReta0';
+%    ORRsig  = ORRsig';
+%end
 
 %%%%% The fault specific parameters %%%%%
 
 if fault_type == 1;  		%%%%% Strike Slip [eqn. (25)]
 
-  f1 = (sig .* q)  .* ORReta + theta + I1 .* sinDelta;
-  f2 = (ytil .* q) .* ORReta + (q .* cosDelta) .* OReta + I2 .* sinDelta;
-  f3 = (dtil .* q) .* ORReta + (q .* sinDelta) .* OReta + I4 .* sinDelta;
-
+  f1 = (sig .* q)  .* ORReta + theta + I1 .* sind;
+  f2 = (ytil .* q) .* ORReta + (q .* cosd) .* OReta + I2 .* sind;
+  f3 = (dtil .* q) .* ORReta + (q .* sind) .* OReta + I4 .* sind;
+%  f3 = -f3; %fix by rlohman?
 elseif fault_type == 2;  	%%%%% Dip Slip  [eqn. (26)]
 
   f1 = q./R - I3 .* cssnd;
-  f2 = (ytil .* q) .* ORRsig + cosDelta .* theta - I1 .* cssnd;
-  f3 = (dtil .* q) .* ORRsig + sinDelta .* theta - I5 .* cssnd;
+  f2 = (ytil .* q) .* ORRsig + cosd .* theta - I1 .* cssnd;
+  f3 = (dtil .* q) .* ORRsig + sind .* theta - I5 .* cssnd;
 
-elseif fault_type == 3;		%%%%% Tensile [eqn. (27)]
+else fault_type == 3;		%%%%% Tensile [eqn. (27)]
 
-  f1 = q.^2 .* ORReta - I3 .* sinDelta2;
+  f1 = q.^2 .* ORReta - I3 .* sind2;
   f2 = (-dtil .* q) .* ORRsig ...
-     - sinDelta .* ((sig .* q) .* ORReta - theta) - I1 .* sinDelta2;
+     - sind .* ((sig .* q) .* ORReta - theta) - I1 .* sind2;
   f3 = (ytil .* q) .* ORRsig ...
-     + cosDelta .* ((sig .* q) .* ORReta - theta) - I5 .* sinDelta2;
-
+     + cosd .* ((sig .* q) .* ORReta - theta) - I5 .* sind2;
+ % f3=-f3;%fix by rlohman?
 end
+
