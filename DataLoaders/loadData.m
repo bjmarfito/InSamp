11f365c1ce9834160d417da387b7cc04e3586ab7
bjmarfito <bryan@eos-bryanMarfito14268.local>
2022-12-26 10:19:08 +0800

    Added timeseries codes

--- DataLoaders/loadData.m
+++ DataLoaders/loadData.m
@@ -23,11 +23,17 @@ switch processor
         datastruct = loadMINTPYISCE(datafilename,zone,limitny,azo);
         datastruct = loadLOS_MINTPYISCE(datastruct,losfilename,azo,iscestack);
 
+    case 'TimeSeries'
+        %This expects time series files in geocoded, geotiff format, in
+        %units of mm/yr.
+        %LOSFILENAME should be two files, look.tif and heading.tif:
+        %{look.tif, heading.tif'}
+        datastruct = loadTimeSeries(datafilename,losfilename,dt, zone);
+
     %Need to convert them to meters, positive means towards the satellite, negative means away from the satellite
     case 'ISCE_MAI'
         datastruct = loadISCE_MAI(datafilename, zone, limitny, azo);
-        datastruct = loadLOS_ISCE_MAI(datastruct,losfilename,azo);
-        
+        datastruct = loadLOS_ISCE_MAI(datastruct,losfilename,azo);       
 end
 
         
