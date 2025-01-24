44c23bbf9ad5b41373dfb9416499a76d03a7ee88
bjmarfito <bryan@Bryan-MacStudio.local>
2025-01-24 11:18:30 +0800

    Added new files for fim

---
+++ ThirdPartyTools/extractCovInsamp.m
@@ -0,0 +1,33 @@
+%% Extract covariance matrix from InSamp
+%% Author: Bryan Marfito
+%%
+%% Usage: extractCovInsamp(InSamp file outputs)
+%% Example: extractCovInsamp({'S1_des.mat', 'A2_asc.mat'})
+
+function [] = extractCovInsamp(fileNames)
+
+
+
+clc
+close all
+dataSets = fileNames;
+
+% Extracts the covariance matrix from InSamp
+covIndex = [];
+sizeCell = 0;
+sizeCellA = 1;
+for i =1:length(dataSets)
+    tmp = load(dataSets{i});
+    covIndex = [covIndex {tmp.savestruct.covstruct.cov}];
+    sizeCell = sizeCell + size(covIndex{i},2);
+    covMatrix(sizeCellA:sizeCell, sizeCellA:sizeCell) = covIndex{i};
+    sizeCellA = sizeCell + 1;
+end
+
+save covMatrix covMatrix -v7.3
+writematrix(covMatrix, "covMatrix.txt", 'Delimiter','space')
+
+disp("Apply Cholesky decomposition and extract the lower triangular matrix factor.")
+disp("Then, get the diagonal values during modelling.")
+
+end
